82efe477603b88161d87306d48683ef3879a3517
Muhammad Ahsan <ahsan.dev.404@gmail.com>
2023-10-17 22:00:59 +0500

    user api done

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 import express from "express"
 import dotenv from 'dotenv'
-
+import errorMiddleware from "./src/middlewares/errorMiddleware.js";
+import cookieParser from "cookie-parser";
+import userRouter from "./src/routes/userRouter.js";
 dotenv.config()
 const app = express()
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({extended:true}))
+
+app.use(`/api/v1`, userRouter);
+
+
+app.use(errorMiddleware);
 
 
 export default app
---
+++ src/routes/userRouter.js
@@ -0,0 +1,41 @@
+import express from "express";
+
+import {
+  forgotPassword,
+  getUserDetails,
+
+  login,
+  logout,
+  register,
+  resetPassword,
+  verifyEmailRequest,
+} from "../controllers/userController.js";
+import {
+  forgetPasswordEmailValidation,
+  loginValidation,
+  registerValidation,
+  resetPasswordValidation,
+} from "../middlewares/formValidation.js";
+import { isAuthenticate } from "../middlewares/auth.js";
+import { verifyEmail } from "../controllers/userController.js";
+import { isVerified } from "../middlewares/auth.js";
+
+const userRouter = express.Router();
+// google authentication setup
+
+
+// local authentication setup
+userRouter.route("/register").post(registerValidation, register);
+userRouter.route("/login").post(loginValidation, login);
+userRouter.route("/logout").get(isAuthenticate, logout);
+userRouter.route("/me").get(isAuthenticate, getUserDetails);
+userRouter.route("/verify/email").get(isAuthenticate, verifyEmailRequest);
+userRouter.route("/verify/email/:token").get(verifyEmail);
+userRouter
+  .route("/password/reset")
+  .post(forgetPasswordEmailValidation, forgotPassword);
+userRouter
+  .route("/password/reset/:token")
+  .post(resetPasswordValidation, resetPassword);
+
+export default userRouter;
